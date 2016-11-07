c4286db88dfc152e7fa41e32d9ea63f33ddb7e11
Kris Szafranski <kdszafranski@gmail.com>
2016-11-07 11:58:14 -0600

    added overwrite

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
   // $.data can read HTML5 data- attributes
   $(".thing-with-data").on("click", function() {
     console.log( $(this).data("potato") );
+
+    // overwrite value of this property
+    $(this).data("potato", "Bork!");
+
   });
 
 });
